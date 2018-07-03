003d749c5c3e4961aed759976e48f785a5bf0e2d
David Zong <davidzong1@gmail.com>
2018-07-03 17:45:03 -0500

    added experiment reader function file and commented on what it should do

---
+++ experiment_reader.m
@@ -0,0 +1,3 @@
+% This function takes in an experimental parameter excel spreadsheet and
+% returns a structure with all of the information needed for doing proper
+% looping or statistics on the experiment
