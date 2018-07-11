09aec79bda8819bae6efd4450f22659a6d55d106
David Zong <davidzong1@gmail.com>
2018-07-11 12:04:49 -0500

    getting started on issue #10

--- experiment_reader.m
+++ experiment_reader.m
@@ -1,3 +1,17 @@
-% This function takes in an experimental parameter excel spreadsheet and
-% returns a structure with all of the information needed for doing proper
-% looping or statistics on the experiment
+clc
+experiment_path = 'C:\Users\david\OneDrive\Plate Reader Data\Experiment Well Maps';
+file = 'IPTG_Experiment_1.xlsx';
+% File I/O, query user if datapath is left blank or invalid
+while exist(experiment_path, 'file') ~= 7
+    prompt = 'Enter path to .xlsx files (include single quotes '') > ';
+    experiment_path = input(prompt) % do not supress with ; since this is user input
+    if exist(experiment_path, 'file') ~= 7
+        disp('Invalid path, enter new path')
+    end
+end
+addpath(experiment_path)
+[~,~,raw] = xlsread(file, 'MAP');
+
+% find blank wells
+blank_wells = find(strcmp(raw, 'BLANK'));
+% convert to wellMap indexing
